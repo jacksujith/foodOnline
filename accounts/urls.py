4e88f61d6743775a1485a2cbf582e998c1734836
sabari <sabaricseng@gmail.com>
2025-05-12 16:54:34 +0530

    vendor and customer dashboard edits

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path   
+from django.urls import path, include
 from .import views
 
 
 
 urlpatterns = [
-
+    path('',views.myAccount),
     path("registerUser/",views.registerUser,name="registerUser"),
     path("registerVendor/",views.registerVendor,name="registerVendor"),
 
@@ -19,7 +19,7 @@ urlpatterns = [
     path('reset_password_validate/<uidb64>/<token>',views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/',views.reset_password, name='reset_password'),
 
-
+    path('vendor/',include('vendor.urls'))
 
 ] 
 
--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,8 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 #Restrict the vendor form accessing the customer page:
+from vendor.models import Vendor
+
 
 def check_role_vendor(user):
     if user.role == 1:
@@ -194,14 +196,17 @@ def myAccount(request):
     redirecturl = detectUser(user)    
     return redirect(redirecturl)
 
+
 @login_required(login_url = 'login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
+
 @login_required(login_url = 'login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
+
     return render(request, 'accounts/vendorDashboard.html')
 
 
